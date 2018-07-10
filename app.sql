a520f1583fa81bc24ec23a35d76c5bc2ebd70dc4
makura <makura6706@gmail.com>
2018-07-10 10:22:57 +0900

    sqlファイルを修正

--- app.sql
+++ app.sql
@@ -1,13 +1,11 @@
-CREATE DATABASE user;
-
-CREATE TABLE user.user_info (
+CREATE TABLE user_info (
 	user_id VARCHAR(255) PRIMARY KEY,
 	user_password VARCHAR(255) NOT NULL,
 	user_name VARCHAR(255) NOT NULL,
 	create_date DATETIME NOT NULL
 );
 
-CREATE TABLE user.chat (
+CREATE TABLE chat (
 	chat_name VARCHAR(255) PRIMARY KEY,
 	chat_table_name VARCHAR(255)  UNIQUE NOT NULL
 );
